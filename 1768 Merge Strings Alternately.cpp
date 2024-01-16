3dabdc3ae58f3c046d30d825f12d9fc5287f8f77
Hitesh Sharma <30732818+hitesh08@users.noreply.github.com>
2024-01-16 20:57:16 +0530

    Solution 1768 Merge Strings Alternately.cpp

---
+++ 1768 Merge Strings Alternately.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/merge-strings-alternately/description/
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int len1 = word1.length();
+        int len2 = word2.length();
+        string result("");
+        int i=0;
+        while(i<len1 || i <len2){
+            if(i<len1) result += word1[i];
+            if(i<len2) result += word2[i];
+            i++;
+        }
+        return result;
+    }
+};
